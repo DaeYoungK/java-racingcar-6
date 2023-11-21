8074fd716aa4cc61361015f760bae00be50bbd99
DaeYoungK <hanguk830@naver.com>
2023-11-22 00:01:31 +0900

    fix(Car): 자동차 객체에 자동차 이름 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,15 +4,18 @@ public class Car {
 
     private static final int INIT_CAR_LEVEL = 0;
     private static final String INIT_CAR_LOG = "";
+
+    private String carName;
     private int level;
     private String log;
 
-    private Car(int level, String log) {
+    private Car(String carName, int level, String log) {
+        this.carName = carName;
         this.level = level;
         this.log = log;
     }
 
-    public static Car create() {
-        return new Car(INIT_CAR_LEVEL, INIT_CAR_LOG);
+    public static Car create(String carName) {
+        return new Car(carName, INIT_CAR_LEVEL, INIT_CAR_LOG);
     }
 }
