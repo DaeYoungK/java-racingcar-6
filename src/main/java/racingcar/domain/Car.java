8e69d9664ae0b2d8de70a43cffe4ab9fb5e982dd
DaeYoungK <hanguk830@naver.com>
2023-11-22 00:31:24 +0900

    feat(Car): 랜덤 숫자를 받아 기준이상이면 레벨이 상승하는 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,25 +2,33 @@ package racingcar.domain;
 
 public class Car {
 
+    private static final int STANDARD_MOVE_NUMBER = 4;
     private static final int INIT_CAR_LEVEL = 0;
-    private static final String INIT_CAR_LOG = "";
 
     private String carName;
     private int level;
-    private String log;
 
-    private Car(String carName, int level, String log) {
+    private Car(String carName, int level) {
         this.carName = carName;
         this.level = level;
-        this.log = log;
     }
 
 
     public static Car create(String carName) {
-        return new Car(carName, INIT_CAR_LEVEL, INIT_CAR_LOG);
+        return new Car(carName, INIT_CAR_LEVEL);
     }
 
     public String getCarName() {
         return carName;
+    }
+
+    public void move(int number) {
+        if (isMove(number)) {
+            level++;
+        }
+    }
+
+    private boolean isMove(int number) {
+        return number >= STANDARD_MOVE_NUMBER;
     }
 }
