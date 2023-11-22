e9d891d83a903f701c10aa5e6c6818082fc62248
DaeYoungK <hanguk830@naver.com>
2023-11-23 00:37:47 +0900

    test(Validator): 자동차 이름이 5글자 이하인지 검증, 빈칸 제거하는 기능 테스트

--- src/test/java/racingcar/validation/ValidatorTest.java
+++ src/test/java/racingcar/validation/ValidatorTest.java
@@ -1,12 +1,14 @@
 package racingcar.validation;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
 
@@ -26,6 +28,24 @@ class ValidatorTest {
     void validSizeTest(String value) {
         assertThatThrownBy(() -> validator.validCount(value))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("경주용 자동차 이름이 5글자 이하인지 검증")
+    @Test
+    void validNameLengthTest() {
+        List<String> value = Arrays.asList("김대영", "Qwe", "으라차차차차");
+        assertThatThrownBy(() -> validator.validName(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("경주용 자동차 이름에 빈칸 제거")
+    @Test
+    void validRemoveBlankTest() {
+        List<String> value = Arrays.asList("김 대 영", "Q e  ", " 으  ");
+
+        List<String> resultValues = validator.validName(value);
+
+        assertThat(resultValues).containsExactly("김대영", "Qe", "으");
     }
 
 
