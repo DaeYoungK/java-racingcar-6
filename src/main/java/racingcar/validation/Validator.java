0137f64b633051683f3264137e07672767ce06b8
DaeYoungK <hanguk830@naver.com>
2023-11-23 00:37:25 +0900

    feat(Validator): 자동차 이름이 5글자 이하인지 검증, 빈칸 제거하는 기능 추가

--- src/main/java/racingcar/validation/Validator.java
+++ src/main/java/racingcar/validation/Validator.java
@@ -1,16 +1,48 @@
 package racingcar.validation;
 
-import racingcar.domain.ErrorMessage;
+import java.util.ArrayList;
+import java.util.List;
 
 import static racingcar.domain.ErrorMessage.*;
 
 public class Validator {
 
     private static final int ZERO = 0;
+    private static final int STANDARD_NAME_LENGTH = 5;
 
-    public void validCount(String count) {
+    public int validCount(String count) {
         int resultCount = checkType(count);
         checkZero(resultCount);
+
+        return resultCount;
+    }
+
+    public List<String> validName(List<String> names) {
+        checkLength(names);
+
+        return removeBlank(names);
+    }
+
+    private List<String> removeBlank(List<String> names) {
+        List<String> resultNames = new ArrayList<>();
+        names.stream().forEach(name -> {
+            resultNames.add(name.replaceAll(" ", ""));
+        });
+
+        return resultNames;
+    }
+
+    private void checkLength(List<String> names) {
+        names.stream().forEach(name -> {
+            if (isLength(name)) {
+                message(LENGTH_ERROR);
+                throw new IllegalArgumentException();
+            }
+        });
+    }
+
+    private boolean isLength(String name) {
+        return name.length() > STANDARD_NAME_LENGTH;
     }
 
     private void checkZero(int count) {
