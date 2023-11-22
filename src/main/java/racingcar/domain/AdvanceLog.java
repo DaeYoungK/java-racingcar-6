18648016f5dbe81efdd181c722f508630ec8a02d
DaeYoungK <hanguk830@naver.com>
2023-11-22 23:05:26 +0900

    fix(Advance): 우승자가 한명일 때 가져오는 인덱스 번호 변경

--- src/main/java/racingcar/domain/AdvanceLog.java
+++ src/main/java/racingcar/domain/AdvanceLog.java
@@ -8,19 +8,21 @@ public class AdvanceLog {
     private static final String INIT_LOG = "";
     private static final String LINE = " : ";
     private static final String SEPARATOR = ", ";
+    private static final String NEXT_LINE = "\n";
     private static final int ONLY_ONE_WINNER = 1;
+    private static final int ONLY_ONE_WINNER_INDEX = 0;
 
     public String make(String carName, int level) {
         String log = INIT_LOG;
         for (int i = 0; i < level; i++) {
             log += ADVANCE_CHECK;
         }
-        return carName + LINE + log;
+        return carName + LINE + log + NEXT_LINE;
     }
 
     public String winners(List<String> winners) {
         if (winners.size() == ONLY_ONE_WINNER) {
-            return winners.get(ONLY_ONE_WINNER);
+            return winners.get(ONLY_ONE_WINNER_INDEX);
         }
 
         return String.join(SEPARATOR, winners);
