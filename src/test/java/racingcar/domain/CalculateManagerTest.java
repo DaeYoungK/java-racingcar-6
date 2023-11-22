68516e2cfe4a786d4ce81193e5402bd4bce9aac5
DaeYoungK <hanguk830@naver.com>
2023-11-22 22:30:43 +0900

    test(CalculateManager): 자동차 목록을 받아 우승자를 구하는 기능 테스트

--- src/test/java/racingcar/domain/CalculateManagerTest.java
+++ src/test/java/racingcar/domain/CalculateManagerTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -21,6 +22,26 @@ class CalculateManagerTest {
 
         assertThat(carNames.size()).isEqualTo(3);
         assertThat(carNames).containsExactly("bong", "car", "apple");
+    }
+
+    @DisplayName("경주 결과를 받아 우승자를 찾는다.")
+    @Test
+    void getWinnersTest() {
+        CarStore carStore = new CarStore();
+        carStore.add("bong");
+        carStore.add("bong2");
+        carStore.add("bong3");
+        List<Car> cars = carStore.getCars();
+        cars.get(0).move(4);
+        cars.get(0).move(4);
+        cars.get(1).move(4);
+        cars.get(1).move(4);
+
+        List<String> winners = calculateManager.getWinners(cars);
+
+        assertThat(winners.size()).isEqualTo(2);
+        assertThat(cars.get(0).getLevel()).isEqualTo(2);
+        assertThat(winners).contains("bong", "bong2");
     }
 
 }
