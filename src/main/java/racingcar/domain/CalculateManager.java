ec28735e7c6e9180d3c3b7db1ab40c32b250805f
DaeYoungK <hanguk830@naver.com>
2023-11-22 22:28:35 +0900

    feat(CalculateManager): 자동차 목록을 받아 우승자를 구하는 기능 추가

--- src/main/java/racingcar/domain/CalculateManager.java
+++ src/main/java/racingcar/domain/CalculateManager.java
@@ -1,11 +1,32 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CalculateManager {
 
+    private static final int OR_ELSE_NUMBER = 0;
+
     public List<String> makeList(String names) {
         return Arrays.stream(names.split(",")).toList();
+    }
+
+    public List<String> getWinners(List<Car> cars) {
+
+        int maxLevel = getMaxLevel(cars);
+
+        return setWinners(cars, maxLevel);
+    }
+
+    private List<String> setWinners(List<Car> cars, int maxLevel) {
+        List<String> carNames = new ArrayList<>();
+        cars.stream().filter(car -> car.getLevel() == maxLevel).forEach(car -> carNames.add(car.getCarName()));
+
+        return carNames;
+    }
+
+    private int getMaxLevel(List<Car> cars) {
+        return cars.stream().mapToInt(v -> v.getLevel()).max().orElse(OR_ELSE_NUMBER);
     }
 }
